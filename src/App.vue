3d373f922fd4f0858cb145da760d9c2bb8b70e59
sniqa <sniqasystem@outlook.com>
2020-11-18 22:39:24 +0800

    start

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div id="app" class="container">
+  <ColumnList :list="list"></ColumnList>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import ColumnList, { ColumnPorps } from './components/ColumnList.vue'
 
+const testData: Array<ColumnPorps> = [
+   {
+    id: 1,
+    title: 'hello',
+    avatar: 'sgs',
+    desc: 'sgs' 
+  },
+  {
+    id: 2,
+    title: 'world',
+    avatar: 'sghs',
+    desc: 'shswh' 
+  },
+  {
+    id: 2,
+    title: 'shshssgww',
+    // avatar: 'sghs',
+    desc: 'shswh' 
+  },
+  {
+    id: 2,
+    title: 'world',
+    avatar: 'sghs',
+    desc: 'shswh' 
+  }
+]
+ 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup(){
+    return {
+      list: testData
+    }
   }
 });
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="row">
+    <div v-for="item in list" :key="item.id" class="col-4">
+      <div class="card h-100 shadow-sm">     
+        <div class="card-body text-center">
+          <img :src="item.avatar" :alt="item.title" class="card-img-top">
+          <h5 class="card-title">{{ item.title }}</h5>
+          <p class="card-text">{{ item.desc }}</p>
+          <a href="" class="btn btn-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import{ defineComponent, PropType } from 'vue'
+export interface ColumnPorps {
+  id: number;
+  title: string;
+  avatar?: string;
+  desc: string;
+}
+export default defineComponent({
+  name : 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<Array<ColumnPorps>>,
+      required: true,
+      default: () => {
+        return{
+          id: 1,
+          title: 'test',
+          avatar: '2tt',
+          desc: 'shswhshw'
+        }
+      }
+    }
+  },
+  setup(props){
+    
+  }
+})
+</script>
+
+<style>
+
+</style>
